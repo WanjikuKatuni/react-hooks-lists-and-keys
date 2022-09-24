7a4292d2092bb387ce471ad31925a8d4a6258222
wanjiku <annritawanjiku77@gmail.com>
2022-09-24 12:49:52 +0300

    completed assignment

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,26 +1,70 @@
+// import React from "react";
+
+// function ColorList() {
+//   const colors = [
+//     "firebrick",
+//     "rebeccapurple",
+//     "salmon",
+//     "darkslategray",
+//     "hotpink",
+//     "grey"
+//   ];
+
+
+//   return (
+//     <div>
+//       <h1>Top 5 CSS Colors</h1>
+//       <ol>
+//         <li style={{ color: colors[0] }}>{colors[0]}</li>
+//         <li style={{ color: colors[1] }}>{colors[1]}</li>
+//         <li style={{ color: colors[2] }}>{colors[2]}</li>
+//         <li style={{ color: colors[3] }}>{colors[3]}</li>
+//         <li style={{ color: colors[4] }}>{colors[4]}</li>
+//         <li style={{ color: colors[5] }}>{colors[5]}</li>
+//       </ol>
+//     </div>
+//   );
+// }
+
+// export default ColorList;
+
+
+
 import React from "react";
 
-function ColorList() {
+function ColorList(){
+
   const colors = [
     "firebrick",
     "rebeccapurple",
     "salmon",
     "darkslategray",
     "hotpink",
+    "blue",
+    "black",
+    "brown",
+    "green"
   ];
 
-  return (
+  const colorElements = colors.map((color)=> {
+
+    // key is added to help react keep track of each element in the array
+    // anytime using an array of jsx elements use, the key prop
+    return <li key={color} style={{color:color}}>{color}</li>
+  });
+
+  return(
+
     <div>
-      <h1>Top 5 CSS Colors</h1>
+      <h1>Top 5 css colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+        {/* display array of list elements here */}
+
+        {colorElements}
       </ol>
     </div>
-  );
+  )
 }
+
 
 export default ColorList;
